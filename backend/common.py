e01959c7317507f97907d16a18a9cf54b08d5980
OthLah001 <oth.lahrimi1996@gmail.com>
2022-11-28 21:22:13 +0000

    feat: create and init the project

---
+++ backend/common.py
@@ -0,0 +1,18 @@
+from users.models import AppUser
+from rest_framework.permissions import BasePermission
+import jwt
+from django.conf import settings
+
+class IsAuthenticate(BasePermission):
+    def has_permission(self, request, view):
+        token = view.kwargs.get('token', None)
+        is_allowed = False
+        try:
+            if token:
+                decoded_token = jwt.decode(token, settings.JWT_KEY, algorithms=[settings.JWT_ALGORITHM])
+                user = AppUser.objects.filter(id=decoded_token['user_id'])
+                is_allowed = user.exists()
+        except:
+            pass
+
+        return is_allowed
